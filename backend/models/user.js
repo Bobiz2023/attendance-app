5d3b8a8e9663fcc5577cd09b62a10639e61540dc
Bobiz2023 <127542727+Bobiz2023@users.noreply.github.com>
2025-04-11 12:38:56 +0100

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
 
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['admin', 'parent'], required: true },
+});
+
+module.exports = mongoose.model('User', UserSchema);

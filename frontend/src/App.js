68670f27ff686ed6cb134930dcfd678f596554a7
Bobiz2023 <127542727+Bobiz2023@users.noreply.github.com>
2025-04-11 12:40:16 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AdminDashboard from './components/AdminDashboard';
+import ParentDashboard from './components/ParentDashboard';
+import LoginForm from './components/LoginForm';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/admin" component={AdminDashboard} />
+          <Route path="/parent" component={ParentDashboard} />
+          <Route path="/" component={LoginForm} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
 
